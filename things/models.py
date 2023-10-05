291c41ffb5a118308a8f4b1ba998006b2b1c0a0d
Aaliyah Merchant <aaliyah.merchant@kcl.ac.uk>
2023-10-05 19:41:06 +0100

    Test

--- things/models.py
+++ things/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.db.models import Model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(Model):
+class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
